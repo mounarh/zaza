08198f4a466b712dc08dc9451ec2316df78501d4
S26poste07\IPI <haddou.0014@gmail.com>
2018-11-07 12:43:16 +0100

    commit1

---
+++ src/main/java/zaza/hello.java
@@ -0,0 +1,9 @@
+package zaza;
+
+public class hello {
+	public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+	
+}
